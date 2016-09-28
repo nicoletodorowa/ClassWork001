809f59a2bf66251fb731767b4751ee21294121b4
nicoletodorowa <nicole.todorowa@gmail.com>
2016-09-28 18:11:13 +0300

    Code refactoring

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-System.out.println("Hello world!");
+		
+		System.out.println("Hello world!");
+		
 	}
 
 }
